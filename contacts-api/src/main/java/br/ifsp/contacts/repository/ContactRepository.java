04fd5f8f50dc23c8b91ab477b278ecda6296f42b
Gabrielxsouza <gabrielxsouzacosta@gmail.com>
2025-09-20 19:19:16 -0300

    setting

---
+++ contacts-api/src/main/java/br/ifsp/contacts/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package br.ifsp.contacts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.ifsp.contacts.model.Contact;
+
+/**
+ * Esta interface extende JpaRepository, que nos fornece métodos prontos 
+ * para acessar e manipular dados no banco de dados. Basta especificar 
+ * a classe de entidade (Contact) e o tipo da chave primária (Long).
+ */
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    // Podemos adicionar métodos personalizados se necessário.
+}
